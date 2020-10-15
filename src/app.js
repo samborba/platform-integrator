5ec0afa0e5e8d90f7123a74a0ca104e9b6e8ec6b
samborba <bf_samuel@hotmail.com>
2020-10-15 11:31:16 -0300

    Enable Cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
